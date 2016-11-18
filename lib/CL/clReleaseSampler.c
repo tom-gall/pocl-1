5aafcaeb6171c942b7b79459d178a356fd2c6465
Tom Gall <tom.gall@linaro.org>
2016-11-18 12:50:04 -0600

    Add the POCL_OBJECT field to the _cl_sampler structure
    Implement the release and retain cycle for cl_sampler

--- lib/CL/clReleaseSampler.c
+++ lib/CL/clReleaseSampler.c
@@ -3,6 +3,14 @@ extern CL_API_ENTRY cl_int CL_API_CALL
 POname(clReleaseSampler)(cl_sampler sampler)
 CL_API_SUFFIX__VERSION_1_0
 {
+  int new_refcount;
+
+  POCL_RETURN_ERROR_COND((sampler == NULL), CL_INVALID_SAMPLER);
+  POCL_RELEASE_OBJECT (sampler, new_refcount);
+
+  if (new_refcount == 0)
+    POCL_MEM_FREE(sampler);
+  
   return CL_SUCCESS;
 }
 POsym(clReleaseSampler)
--- lib/CL/clRetainSampler.c
+++ lib/CL/clRetainSampler.c
@@ -3,7 +3,8 @@
 CL_API_ENTRY cl_int CL_API_CALL
 POname(clRetainSampler)(cl_sampler  sampler ) CL_API_SUFFIX__VERSION_1_0
 {
-  POCL_ABORT_UNIMPLEMENTED("The entire clRetainSampler call");
+  POCL_RETURN_ERROR_COND((sampler == NULL), CL_INVALID_SAMPLER);
+  POCL_RETAIN_OBJECT (sampler);
   return CL_SUCCESS;
 }
 
